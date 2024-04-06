79188da96077598cec346de64a343a3b6fd4ce62
mlieberbaum <79414109+mlieberbaum@users.noreply.github.com>
2024-04-06 15:02:32 -0400

    Adding script to compare linear and nonlinear control laws

---
+++ ControlLawSim/linearCtrlSim.m
@@ -0,0 +1,137 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% This script compares the nonlinear control law from the reference below
+% to two linear control laws.
+% 
+% Wie, B., Weiss, H., and Arapostathis, A., “Quaternion Feedback Regulator 
+% for Spacecraft Eigenaxis Rotations,” Journal of Guidance, Control, and 
+% Dynamics, Vol. 12, No. 3, 1989, pp. 375-380.doi: 10.2514/3.20418
+% 
+% The simulation input conditions are:
+% 
+% Spacecraft Inertia Matrix (kg m^2):
+% 
+%   J = [1200   100  -200
+%         100  2200   300
+%        -200   300  3100] * 0.9
+% 
+% Initial quaternion (note: this was normalized to ensure magnitude 1):
+%
+%   q0 = [0.57, 0.57, 0.57, 0.159]
+%
+% Commanded quaternion:
+%
+%   qc = [0, 0, 0, 1]
+%
+% Initial body rates (rad/s):
+%
+%   w0 = [0.01, 0.01, 0.01]
+%
+% Estimated inertia (10% error, zero diagonal elements assumed):
+%
+%   Jest = [1200     0     0
+%              0  2200     0
+%              0     0  3100]
+%
+% Control gain matrix D:
+%
+%   D = [379.2       0       0
+%            0   695.2       0
+%            0       0   979.6]
+%
+% Gyroscopic torque offset scale factor:
+%
+%   mu = 0.9
+% 
+% For the linear PD controller:
+%
+%   Kp = 30.0
+%   Kd = 379.2
+%
+% For the linear PID controller:
+%
+%   Kp = 89.91
+%   Kd = 568.8
+%   Ki = 4.74
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+clear
+close all
+clc
+
+
+addpath(genpath('.\Dynamics'))
+addpath(genpath('.\Functions'))
+addpath(genpath('.\InputFiles'))
+
+
+
+%% RUN THE SIMULATIONS
+
+% PD Controller
+inputData = LinearControlLaw('PD');
+data{1} = ControlLawSim(inputData);
+
+% PID Controller
+inputData = LinearControlLaw('PID');
+data{2} = ControlLawSim(inputData);
+
+% Nonlinear control law
+inputData = BongWieDiscrete();
+inputData.ctrl.K = diag([110 110 110]);
+data{3} = ControlLawSim(inputData);
+
+
+
+figure
+set(gcf, 'Color', 'w')
+set(gcf, 'Position', [640 700 730 530]);
+
+tiledlayout(2, 2, 'TileSpacing', 'compact', 'Padding', 'compact');
+
+for idx = 1:4
+
+    nexttile;
+    hold on
+    grid on
+
+    for idx2 = 1:3
+        plot(data{idx2}.t, data{idx2}.q(:,idx));
+    end
+
+    xlabel('Time (sec)')
+    ylabel(['q', num2str(idx)])
+    legend('PD', 'PID', 'Nonlinear', 'Location', 'Best')
+
+end
+
+saveas(gcf, 'LinCtrlSimQ.svg', 'svg')
+
+
+%% BODY RATE PLOT
+
+figure
+set(gcf, 'Color', 'w')
+set(gcf, 'Position', [640 700 730 530]);
+
+tiledlayout(2, 2, 'TileSpacing', 'compact', 'Padding', 'compact');
+
+for idx = 1:3
+
+    nexttile;
+    hold on
+    grid on
+
+    for idx2 = 1:3
+        plot(data{idx2}.t, data{idx2}.w(:,idx));
+    end
+
+    xlabel('Time (sec)')
+    ylabel(['\omega', num2str(idx)])
+    legend('PD', 'PID', 'Nonlinear', 'Location', 'Best')
+
+end
+
+saveas(gcf, 'LinCtrlSimOmega.svg', 'svg')
